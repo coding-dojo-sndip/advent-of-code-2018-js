846352bb9fc9782295da5073c02780425ab2403f
Romain Warnan <romain.warnan@insee.fr>
2019-06-11 10:59:57 +0200

    Correction de la méthode arrayOfLines

--- src/main/days.js
+++ src/main/days.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
 
-export const arrayOfLines = input => fs.readFileSync(input, 'utf-8').split('\n')
+export const arrayOfLines = input => fs.readFileSync(input, 'utf-8')
+    .split('\n')
+    .map(line => line.replace('\r', ''))
 
 export const arrayOfInts = input => arrayOfLines(input).map(n => parseInt(n))
