06ecc4cfffb944affbe0ec1b331077ec5fa915d7
Romain Warnan <romain.warnan@insee.fr>
2019-06-12 09:05:05 +0200

    Configuration eslint et adaptation du code en conséquence

--- src/main/days.js
+++ src/main/days.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export const arrayOfLines = input => fs.readFileSync(input, 'utf-8')
-    .split('\n')
-    .map(line => line.replace('\r', ''))
+	.split('\n')
+	.map(line => line.replace('\r', ''))
 
 export const arrayOfInts = input => arrayOfLines(input).map(n => parseInt(n))
--- src/test/day01.test.js
+++ src/test/day01.test.js
@@ -1,25 +1,25 @@
 import {part1, part2} from '../main/day01'
 
 describe('Day 01, part 1', () => {
-    test('the resulting frequency is 3', () => {
-        expect(part1('src/test/resources/01-0.txt')).toBe(3)
-    })
+	test('the resulting frequency is 3', () => {
+		expect(part1('src/test/resources/01-0.txt')).toBe(3)
+	})
     
-    test('the answer to part 1 is 592', () => {
-        const answer = part1('src/main/resources/01.txt')
-        console.log(`Day01.1 : ${answer}`)
-        expect(answer).toBe(592)
-    })
+	test('the answer to part 1 is 592', () => {
+		const answer = part1('src/main/resources/01.txt')
+		console.log(`Day01.1 : ${answer}`)
+		expect(answer).toBe(592)
+	})
 })
 
 describe('Day 01, part 2', () => {
-    test('the first frequency the device reaches twice is 2', () => {
-        expect(part2('src/test/resources/01-0.txt')).toBe(2)
-    })
+	test('the first frequency the device reaches twice is 2', () => {
+		expect(part2('src/test/resources/01-0.txt')).toBe(2)
+	})
     
-    test('the answer to part 2 is 241', () => {
-        const answer = part2('src/main/resources/01.txt')
-        console.log(`Day01.2 : ${answer}`)
-        expect(answer).toBe(241)
-    })
+	test('the answer to part 2 is 241', () => {
+		const answer = part2('src/main/resources/01.txt')
+		console.log(`Day01.2 : ${answer}`)
+		expect(answer).toBe(241)
+	})
 })
