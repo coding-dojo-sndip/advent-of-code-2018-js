8a4c5d8e2cd9e85f07d8155e17b26350eca6473a
Romain Warnan <romain.warnan@insee.fr>
2019-06-27 13:17:10 +0200

    Refactoring day13.1

---
+++ src/main/beans/Wagon.js
@@ -0,0 +1,111 @@
+import { Point } from './Point'
+
+const turns = ['LEFT', 'STRAIGHT', 'RIGHT']
+
+export class Wagon {
+	constructor(x, y, way) {
+		this.point = new Point(x, y)
+		this.way = way
+		this.turnIndex = 0
+	}
+
+	nextTurn() {
+		return (this.turnIndex + 1) % turns.length
+	}
+
+	nextPoint() {
+		switch (this.way) {
+		case '^':
+			return new Point(this.point.x, this.point.y - 1)
+		case 'v':
+			return new Point(this.point.x, this.point.y + 1)
+		case '<':
+			return new Point(this.point.x - 1, this.point.y)
+		case '>':
+			return new Point(this.point.x + 1, this.point.y)
+		}
+	}
+
+	collideWith(other) {
+		return this.way !== other.way && this.point.x === other.point.x && this.point.y === other.point.y
+	}
+
+	collideWithAny(wagons) {
+		return wagons.some(other => this.collideWith(other))
+	}
+
+	move(track) {
+		this.point = this.nextPoint()
+		switch (track[this.point.y][this.point.x]) {
+		case '/':
+			switch (this.way) {
+			case '^':
+				this.way = '>'
+				break
+			case 'v':
+				this.way = '<'
+				break
+			case '<':
+				this.way = 'v'
+				break
+			case '>':
+				this.way = '^'
+				break
+			}
+			break
+		case '\\':
+			switch (this.way) {
+			case '^':
+				this.way = '<'
+				break
+			case 'v':
+				this.way = '>'
+				break
+			case '<':
+				this.way = '^'
+				break
+			case '>':
+				this.way = 'v'
+				break
+			}
+			break
+		case '+':
+			switch (turns[this.turnIndex]) {
+			case 'LEFT':
+				switch (this.way) {
+				case '^':
+					this.way = '<'
+					break
+				case 'v':
+					this.way = '>'
+					break
+				case '<':
+					this.way = 'v'
+					break
+				case '>':
+					this.way = '^'
+					break
+				}
+				break
+			case 'RIGHT':
+				switch (this.way) {
+				case '^':
+					this.way = '>'
+					break
+				case 'v':
+					this.way = '<'
+					break
+				case '<':
+					this.way = '^'
+					break
+				case '>':
+					this.way = 'v'
+					break
+				}
+				break
+			}
+			this.turnIndex = this.nextTurn()
+			break
+		}
+	}
+}
--- src/main/day13.js
+++ src/main/day13.js
@@ -1,116 +1,7 @@
-import { arrayOfLines, Point } from './days'
+import { arrayOfLines } from './days'
+import { Wagon } from './beans/Wagon'
 
 const ways = ['^', 'v', '<', '>']
-const turns = ['LEFT', 'STRAIGHT', 'RIGHT']
-
-class Wagon {
-	constructor(x, y, way) {
-		this.point = new Point(x, y)
-		this.way = way
-		this.turnIndex = 0
-	}
-
-	move(track) {
-		this.point = this.nextPoint()
-		switch (track[this.point.y][this.point.x]) {
-		case '/':
-			switch (this.way) {
-			case '^':
-				this.way = '>'
-				break
-			case 'v':
-				this.way = '<'
-				break
-			case '<':
-				this.way = 'v'
-				break
-			case '>':
-				this.way = '^'
-				break
-			}
-			break
-		case '\\':
-			switch (this.way) {
-			case '^':
-				this.way = '<'
-				break
-			case 'v':
-				this.way = '>'
-				break
-			case '<':
-				this.way = '^'
-				break
-			case '>':
-				this.way = 'v'
-				break
-			}
-			break
-
-		case '+':
-			switch (turns[this.turnIndex]) {
-			case 'LEFT':
-				switch (this.way) {
-				case '^':
-					this.way = '<'
-					break
-				case 'v':
-					this.way = '>'
-					break
-				case '<':
-					this.way = 'v'
-					break
-				case '>':
-					this.way = '^'
-					break
-				}
-				break
-			case 'RIGHT':
-				switch (this.way) {
-				case '^':
-					this.way = '>'
-					break
-				case 'v':
-					this.way = '<'
-					break
-				case '<':
-					this.way = '^'
-					break
-				case '>':
-					this.way = 'v'
-					break
-				}
-				break
-			}
-			this.turnIndex = this.nextTurn()
-			break
-		}
-	}
-
-	nextTurn() {
-		return (this.turnIndex + 1) % turns.length
-	}
-
-	nextPoint() {
-		switch (this.way) {
-		case '^':
-			return new Point(this.point.x, this.point.y - 1)
-		case 'v':
-			return new Point(this.point.x, this.point.y + 1)
-		case '<':
-			return new Point(this.point.x - 1, this.point.y)
-		case '>':
-			return new Point(this.point.x + 1, this.point.y)
-		}
-	}
-
-	collideWith(other) {
-		return this.way !== other.way && this.point.x === other.point.x && this.point.y === other.point.y
-	}
-
-	collideWithAny(wagons) {
-		return wagons.some(other => this.collideWith(other))
-	}
-}
 
 export const part1 = input => {
 	const { track, wagons } = readInput(input)
@@ -125,11 +16,6 @@ export const part1 = input => {
 
 export const part2 = input => '0,0'
 
-const replaceWagonsByTrack = string => {
-	let a = string.replace(/(v|\^)/, '|')
-	return a.replace(/(<|>)/, '-')
-}
-
 const readInput = input => {
 	const track = arrayOfLines(input).map(line => [...line])
 	const wagons = []
@@ -143,4 +29,9 @@ const readInput = input => {
 		}
 	}
 	return { track, wagons }
+}
+
+const replaceWagonsByTrack = string => {
+	let a = string.replace(/(v|\^)/, '|')
+	return a.replace(/(<|>)/, '-')
 }
