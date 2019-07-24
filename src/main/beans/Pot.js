cbabc85b231be2c206d7f741728ab4e3b4ce8640
Romain Warnan <romain.warnan@insee.fr>
2019-07-24 08:56:38 +0200

    Enoncé day12

--- src/main/beans/Pot.js
+++ src/main/beans/Pot.js
@@ -2,21 +2,6 @@ class Pot {
 	constructor(index, state) {
 		this.index = index
 		this.state = state
-		this.nextState = undefined
-	}
-
-	computeNextState(rules, pots) {
-		const index = pots.indexOf(this)
-		const pattern = pots[index - 2].state + pots[index - 1].state + this.state + pots[index + 1].state + pots[index + 2].state
-		this.nextState = rules.includes(pattern) ? '#' : '.'
-	}
-
-	changeState() {
-		this.state = this.nextState || this.state
-	}
-
-	isEmpty() {
-		return this.state === '.'
 	}
 }
 
